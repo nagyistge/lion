90f9299855168458ad8a5e10667554d66440ac69
Matteo Depalo <matteodepalo@gmail.com>
2014-07-26 16:26:10 -0400

    Add pace

--- Brocfile.js
+++ Brocfile.js
@@ -17,4 +17,7 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+app.import('vendor/pace/pace.js');
+app.import('vendor/pace/themes/blue/pace-theme-flash.css');
+
 module.exports = app.toTree();
