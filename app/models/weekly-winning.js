ccf52d7504d2f550f60560fd3669186345de5913
Kaosisochukwu Uzokwe <kuzokwe@gmail.com>
2015-12-18 16:47:02 -0500

    Update the markup for the stats and hall of fame templates

--- app/models/weekly-winning.js
+++ app/models/weekly-winning.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   startDate: DS.attr('date'),
-  winner: DS.belongsTo('user'),
+  winner: DS.belongsTo('user', { async: true }),
   points: DS.attr('number')
 });
